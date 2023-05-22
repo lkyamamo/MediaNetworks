453443e37df7bacaf93bcd7e10b7443e7f81a0c5
Logan Yamamoto <lkyamamo@usc.edu>
2023-05-22 00:48:00 -0700

    have correct implementation of library for duplexes. Library algorithms have issue dealing with multiplexes for greater than 2 layers.

--- test.py
+++ test.py
@@ -5,19 +5,14 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import sys
 import data_graphs.create_graph as cg
+import numpy as np
 
-#This file will take the data that is in the raw_data folder and create the respective networks of the data
-dir_path = os.getcwd() + "/raw_data"
+results_dict = {'a': [1,2,3],
+                'b': [2,3,4],
+                'c': [3,4,5],
+                'd': [4,5,6]}
 
-graphs = []
-
-for file in os.scandir(dir_path):
-    if(file.name[-2:] != "md"):
-
-        graphs.append(cg.CreateGraph(file))
-
-node_list1 = list(graphs[0].graph.nodes)
-node_list2 = list(graphs[1].graph.nodes)
-for i in range(len(node_list1)):
-    if node_list1[i] != node_list2[i]:
-        print("{0} and {1} ".format(node_list1[i], node_list2[i]))
+with open("output.txt","w") as f:
+    for key,value in results_dict.items():
+        f.write("{0} {1}".format(key, value))
+        f.write("\n")
